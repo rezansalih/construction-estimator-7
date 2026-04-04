9b5e06b2932c1f18b7551b05b71da8c6922c793d
rezansalih <Rezansalih41@gmail.com>
2026-04-04 15:04:18 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'estimator-v1';
+const CACHE_NAME = 'estimator-v2';
 
 // We will cache index.html, icons and also cache the external CDN resources so the app works offline.
 const urlsToCache = [
